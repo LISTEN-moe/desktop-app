6318a447aea78ad6034f36a55bf185a18340d9b3
Crawl <icrawltogo@gmail.com>
2018-11-30 00:18:25 +0100

    feat: add build config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,42 @@
 module.exports = {
-	productionSourceMap: false
+	productionSourceMap: false,
+	pluginOptions: {
+		electronBuilder: {
+			builderOptions: {
+				appId: 'com.electron.listen-moe',
+				productName: 'LISTEN.moe - Desktop App',
+				copyright: 'Copyright © 2018-2019 iCrawl',
+				win: {
+					target: [
+						{
+							target: 'portable',
+							arch: [
+								'x64'
+							]
+						}
+					]
+				},
+				mac: {
+					target: [
+						{
+							target: 'dmg',
+							arch: [
+								'x64'
+							]
+						}
+					]
+				},
+				linux: {
+					target: [
+						{
+							target: 'AppImage',
+							arch: [
+								'x64'
+							]
+						}
+					]
+				}
+			}
+		}
+	}
 };
