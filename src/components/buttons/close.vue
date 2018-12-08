e4bc2b5a99b18dc167fb801cab5a07bd052b48e9
Pitu <heyitspitu@gmail.com>
2018-12-08 13:36:54 -0300

    Close button component

---
+++ src/components/buttons/close.vue
@@ -0,0 +1,32 @@
+<style lang="scss" scoped>
+	.closeContainer {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		width: 35px;
+		height: 35px;
+		background: #262838;
+		border-radius: 25px;
+		z-index: 2;
+		-webkit-app-region: no-drag;
+
+		> svg {
+			width: 25px;
+		}
+	}
+</style>
+<template>
+	<div class="closeContainer">
+		<Budicon icon="cross"
+			y="5"
+			:pointer="true"
+			@click.native.stop.prevent="$emit('clicked')" />
+	</div>
+</template>
+
+<script>
+import Budicon from '@/components/icons/budicon';
+export default {
+	components: { Budicon }
+};
+</script>
