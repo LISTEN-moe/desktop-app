f05ac209ed9e561cf7f93f5e15a175c146b46cd6
iCrawl <icrawltogo@gmail.com>
2019-05-12 14:01:57 +0200

    fix: websocket worker reconnection

--- src/assets/worker/websocket.worker.js
+++ src/assets/worker/websocket.worker.js
@@ -1,41 +1,52 @@
-let ws = new WebSocket('wss://listen.moe/gateway');
 let heartbeatInterval;
 
-function heartbeat(interval) {
+function heartbeat(ws, interval) {
 	heartbeatInterval = setInterval(() => {
 		ws.send(JSON.stringify({ op: 9 }));
 	}, interval);
 }
 
-ws.onopen = () => {
-	console.log('%c> Websocket connection established.', 'color: #008000;');
-	clearInterval(heartbeatInterval);
-	ws.send(JSON.stringify({ op: 0, d: { auth: '' } }));
-};
+function connect() {
+	let ws = new WebSocket('wss://listen.moe/gateway');
 
-ws.onmessage = message => {
-	if (!message.data.length) return;
-	let response;
-	try {
-		response = JSON.parse(message.data);
-	} catch (error) {
-		return;
-	}
-	switch (response.op) {
-		case 0:
-			heartbeat(response.d.heartbeat);
-			break;
-		case 1:
-			if (response.t !== 'TRACK_UPDATE' && response.t !== 'TRACK_UPDATE_REQUEST' && response.t !== 'QUEUE_UPDATE' && response.t !== 'NOTIFICATION') break;
-			postMessage(response.d);
-			break;
-		default:
-			break;
-	}
-};
+	ws.onopen = () => {
+		console.log('%c> Websocket connection established.', 'color: #008000;');
+		clearInterval(heartbeatInterval);
+		heartbeatInterval = null;
+	};
 
-ws.onclose = error => {
-	console.log('%c> Websocket connection closed. Reconnecting...', 'color: #ff015b;', error);
-	clearInterval(heartbeatInterval);
-	/* if (!error.wasClean) */ setTimeout(() => ws = new WebSocket('wss://listen.moe/gateway'), 5000);
-};
+	ws.onmessage = message => {
+		if (!message.data.length) return;
+		let response;
+		try {
+			response = JSON.parse(message.data);
+		} catch (error) {
+			return;
+		}
+		switch (response.op) {
+			case 0:
+				ws.send(JSON.stringify({ op: 9 }));
+				heartbeat(ws, response.d.heartbeat);
+				break;
+			case 1:
+				if (response.t !== 'TRACK_UPDATE' && response.t !== 'TRACK_UPDATE_REQUEST' && response.t !== 'QUEUE_UPDATE' && response.t !== 'NOTIFICATION') break;
+				postMessage(response.d);
+				break;
+			default:
+				break;
+		}
+	};
+
+	ws.onclose = error => {
+		console.log('%c> Websocket connection closed. Reconnecting...', 'color: #ff015b;', error);
+		clearInterval(heartbeatInterval);
+		heartbeatInterval = null;
+		if (ws) {
+			ws.close();
+			ws = null;
+		}
+		setTimeout(() => connect(), 5000);
+	};
+}
+
+connect();
